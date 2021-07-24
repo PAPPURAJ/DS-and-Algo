463eda8f6d8dac8a10b465dc4b148de253d7baba
PAPPURAJ BHOTTACHARJEE <45383061+PAPPURAJ@users.noreply.github.com>
2021-07-24 20:24:27 +0600

    Create Doubly Linked list.cpp

---
+++ Doubly Linked list.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define nl '\n'
+
+struct Node{
+    int value;
+    struct Node *next,*prev;
+};
+
+
+struct Node *head;
+
+
+void insertFirst(int val){
+       struct Node *temp=(struct Node *) malloc(sizeof(struct Node*));
+
+       if(head==NULL){
+           temp->prev=NULL;
+           temp->next=NULL;
+           temp->value=val;
+           head=temp;
+       }else{
+           temp->value=val;
+           temp->prev=NULL;
+           temp->next=head;
+           head=temp;
+       }
+
+}
+
+
+void insertLast(int val){
+   struct Node *temp=(struct Node *) malloc(sizeof(struct Node*)),*t2;
+
+    if(head==NULL){
+        temp->next=NULL;
+        temp->prev=NULL;
+        temp->value=val;
+        head=temp;
+    }else{
+        t2=head;
+        temp->value=val;
+
+        while(t2->next!=NULL)
+            t2=t2->next;
+
+        temp->next=NULL;   
+        temp->prev=t2;
+        t2->next=temp;
+    }
+}
+
+
+void display(){
+    struct Node *temp=head;
+    
+    cout<<"Displaying value"<<nl;
+    while(temp!=NULL)
+        {
+            cout<<temp->value<<nl;
+            temp=temp->next;
+        }
+    
+    
+}
+
+int main(){
+    
+    for (size_t i = 0; i < 10; i++)
+    {
+       insertFirst(i);
+    }
+
+    display();
+
+    insertLast(60);
+    display();
+
+    insertFirst(70);
+    display();
+    return 0;
+}
